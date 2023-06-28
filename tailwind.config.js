8bb80a2285a62387cfa92a153b5091ca7191cccc
Craig McNaughton <therealcraigmcn@gmail.com>
2023-06-27 18:54:35 -0700

    Add Speaking Order project
    - update to v1.0.3
    - add Speaking Order project to Netlify
    - add Speaking Order project to Portfolio
      - upload image
    - update Resumé
    - npm update, outdated, audit fix
    - update Tailwind config
    - fix eslint errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    purge: ['./src/**/*.html'],
-    darkMode: false, // or 'media' or 'class'
+    content: ['./src/**/*.html'],
     theme: {
         extend: {
             colors: {
